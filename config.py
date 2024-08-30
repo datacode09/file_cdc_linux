7b0805a1ce14daf00211e257b8e0b04ceb0cc98c
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-08-30 11:52:21 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Configuration for source server
+SRC_USER = "qhg85bx"
+SRC_IP = "strplvarie0002.fg.rbc.com"
+SRC_DIR = "/rbc/home/prieapptom"
+
+# Configuration for destination server
+DEST_USER = "qhg85bx"
+DEST_IP = "guerlvarie0002.fg.rbc.com"
+DEST_DIR = "/rbc/home/prieapptom"
+
+# Other settings
+COMPRESS = True  # Set to True to enable compression
+DELETE_EXTRA_FILES = True  # Set to True to delete files on destination not present on source
